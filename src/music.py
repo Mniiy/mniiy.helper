2bfd17ee645b2dad8dc9b3decdc8b964e9ae7b07
Sophin <sophin.2010.pv@gmail.com>
2025-12-08 20:50:08 -0400

    add queue to music.py

--- src/music.py
+++ src/music.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import yt_dlp, os
+import datetime
 from collections import deque # gonna kms, this code... I hate this code...
 # Why? IDK
 
@@ -13,38 +14,70 @@ ydl_opts = {
             }],
             'quiet': True,
             'progress': True,
+            'writethumbnail': True,
             'no_warnings': True,
         }
+
+class Song:
+    def __init__(self, url:str):
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            self.info= ydl.extract_info(url, download=False)
+            self.url= self.info['url']
+            self.originalUrl= url
+            self.title= self.info['title']
+            self.description= self.info.get('description', None)
+            self.duration = self.info.get('duration', None)
+            self.thumbnail = self.info.get('thumbnail', None)
+    
+    def embed(self) -> discord.Embed:
+        embed= discord.Embed(
+            title=self.title,
+            url=self.originalUrl,
+            description=f"{self.description}\n\nTime: {self.TotalDuration()}"
+        )
+        embed.set_image(url=self.thumbnail)
+        return embed
+    
+    def TotalDuration(self) -> str:
+        if self.duration:
+            t= datetime.timedelta(seconds=self.duration)
+            return f"{int(t.total_seconds() // 60)}:{int(t.total_seconds() % 60):02}"
+        return "00:00"
+
 
 class MusicPlayer:
     def __init__(self, ctx:commands.Context):
         self.ctx= ctx
         self.queue= deque()
-        self.current= None
+        self.current:Song= None
 
     async def playNext(self):
         if len(self.queue) > 0:
             self.current= self.queue.popleft()
             try:
-                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                    ydl.download(self.current['url'])
-                
-                for file in os.listdir("./"):
-                    if file.endswith(".mp3"):
-                        os.rename(file, "song.mp3")
-
                 voice = discord.utils.get(self.ctx.bot.voice_clients, guild=self.ctx.guild)
-                if voice.playing:
+                if voice and voice.is_playing():
                     voice.stop()
-                voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: self.ctx.bot.loop.create_task(self.play_next()))
-
-
-                await self.ctx.send("playing...\nIdk LOL")
+                voice.play(discord.FFmpegPCMAudio(self.current.url), after=lambda e: self.ctx.bot.loop.create_task(self.playNext()))
             except Exception as error:
                 await self.ctx.send(f"what? {error}")
     
-    def addQueue(self, url:str, title:str):
-        self.queue.append({"url": url, "title":title})
+    def Queue(self):
+        tmp= ""
+
+        for song in self.queue:
+            tmp += f"{song.title} - [Url]({song.originalUrl})\n"
+
+        embed=discord.Embed(
+            title="Queue 🎧",
+            description=tmp
+        )
+        embed.set_thumbnail(url=self.current.thumbnail)
+        embed.set_footer(text=f"Current: {self.current.title}")
+        return embed
+
+    def addQueue(self, song:Song):
+        self.queue.append(song)
 
 class music(commands.Cog):
     def __init__(self, bot):
@@ -71,9 +104,10 @@ class music(commands.Cog):
 
     @commands.command(
             name="play",
-            description="Toca música"
+            description="Toca música",
+            aliases=["p", "yt"]
         )
-    async def playCommand(self, ctx:commands.Context, url):
+    async def playCommand(self, ctx:commands.Context, url:str):
         if not ctx.voice_client:
             await ctx.send("I'm not in a vc (UwU)")
             return
@@ -83,16 +117,14 @@ class music(commands.Cog):
 
         player = self.players[ctx.guild.id]
 
-        try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                info = ydl.extract_info(url, download=False)
-                title = info['title']
-                player.addQueue(url, title)
-        except Exception as error:
-            await ctx.send(f"wtf... {error}")
+        song= Song(url)
+        player.addQueue(song)
 
         if not player.current:
             await player.playNext()
+            await ctx.send(embed=song.embed())
+        else:
+            await ctx.send("added to queue")
 
     
     @commands.command(
@@ -108,9 +140,33 @@ class music(commands.Cog):
             self.players[ctx.guild.id] = MusicPlayer(ctx)
 
         player = self.players[ctx.guild.id]
-        player.play_next()
+        player.playNext()
             
-    
+    @commands.command(
+        name="queue",
+        description="Vê a queue)")
+    async def queueCommand(self, ctx:commands.Context):
+        if ctx.guild.id not in self.players:
+            self.players[ctx.guild.id] = MusicPlayer(ctx)
+
+        player = self.players[ctx.guild.id]
+        
+        queue= player.Queue()
+        await ctx.send(embed=queue)
+
+
+    @commands.command(
+        name="playing",
+        aliases=["current", "c"],
+        description="Que música tá tocando?")
+    async def playingCommand(self, ctx:commands.Context):
+        if ctx.guild.id not in self.players:
+            self.players[ctx.guild.id] = MusicPlayer(ctx)
+
+        player = self.players[ctx.guild.id]
+        
+        current= player.current
+        await ctx.send(embed=current.embed())
 
 async def setup(bot):
     await bot.add_cog(music(bot))
